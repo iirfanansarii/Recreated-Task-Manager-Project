b0c4eb4df3329296fe241791b68a38ff26817b32
Irfan Ansari <irfanansari2k20@gmail.com>
2020-10-18 03:13:39 +0530

    Setting Tasks to Completed and Uncompleted - frontend

--- src/app/pages/task-view/task-view.component.ts
+++ src/app/pages/task-view/task-view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { TaskService } from 'src/app/task.service';
+import { Task } from 'src/app/models/task.model';
+import { List } from 'src/app/models/list.model';
 
 @Component({
   selector: 'app-task-view',
@@ -9,22 +11,33 @@ import { TaskService } from 'src/app/task.service';
 })
 export class TaskViewComponent implements OnInit {
   //injecting task service
-  lists: any[];
-  tasks: any[];
+  lists: List[];
+  tasks: Task[];
   constructor(
     private taskService: TaskService,
     private route: ActivatedRoute
   ) {}
 
-  ngOnInit() // after injecting task service creating method
-  {
+  ngOnInit() { // after injecting task service creating method
     this.route.params.subscribe((params: Params) => {
-      this.taskService.getTasks(params.listId).subscribe((tasks: any[]) => {
+      this.taskService.getTasks(params.listId).subscribe((tasks: Task[]) => {
         this.tasks = tasks;
       });
     });
-    this.taskService.getLists().subscribe((lists: any[]) => {
+    this.taskService.getLists().subscribe((lists: List[]) => {
       this.lists = lists;
     });
   }
+
+  onTaskClick(task: Task) {
+    // we want to set the task to completed
+    this.taskService.complete(task).subscribe(() => {
+      // the task has been set to completed successfully
+      console.log('Completed successully!');
+      task.completed = !task.completed;
+    
+    });
+  }
+
+
 }
